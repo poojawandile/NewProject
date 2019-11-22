4c7db8fc8093e60d6071d6cd25df8742361edfba
615528 <615528@CTSCEI1805.cts.com>
2019-11-22 14:21:56 +0100

    First commit1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	
+	public static void main(String [] args) {
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes")
+			System.out.println("eat something .....");
+			else
+			{
+				System.out.println(" Do work....");
+			}
+				
+		
+		
+	}
+
+}
